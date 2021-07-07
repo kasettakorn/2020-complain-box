d0a1eb6ae0438efcfe4f2f47943535799e4f003f
kasettakorn <kasettakorn_techno@hotmail.com>
2021-07-07 19:51:59 +0700

    Updates

---
+++ precache-manifest.b4d83a226cde03bbeced1607201ab7d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd25ac70ac6fdd06356abbea2b75e65",
+    "url": "/tanabata-2021/index.html"
+  },
+  {
+    "revision": "3e233e7395b86a022db0",
+    "url": "/tanabata-2021/static/css/2.71d44871.chunk.css"
+  },
+  {
+    "revision": "607bf2323d6abd85e737",
+    "url": "/tanabata-2021/static/css/main.5561ae39.chunk.css"
+  },
+  {
+    "revision": "3e233e7395b86a022db0",
+    "url": "/tanabata-2021/static/js/2.a393dec5.chunk.js"
+  },
+  {
+    "revision": "b962909a7040b53b76bb931dd59e4242",
+    "url": "/tanabata-2021/static/js/2.a393dec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607bf2323d6abd85e737",
+    "url": "/tanabata-2021/static/js/main.ae01022a.chunk.js"
+  },
+  {
+    "revision": "4144d6af68dbb780ebf0",
+    "url": "/tanabata-2021/static/js/runtime-main.96b5d4a9.js"
+  }
+]);
